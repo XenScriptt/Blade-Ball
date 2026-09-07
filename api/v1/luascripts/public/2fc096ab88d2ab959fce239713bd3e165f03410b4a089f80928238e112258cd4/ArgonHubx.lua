457131d2a517799b9bc2490ab0af97a6d0a54901
XenScriptt <janjanc438@gmail.com>
2026-09-07 19:40:53 +0800

    Update ArgonHubx.lua

--- api/v1/luascripts/public/2fc096ab88d2ab959fce239713bd3e165f03410b4a089f80928238e112258cd4/ArgonHubx.lua
+++ api/v1/luascripts/public/2fc096ab88d2ab959fce239713bd3e165f03410b4a089f80928238e112258cd4/ArgonHubx.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("http://31.172.87.116/9F3kw9fZhbgUTyjestJh7n1iq0F9lsuHORIcpe749Qb9kBonrOgqaQehxu7h964uSw3EqdolFyVmXJn9"))()
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/n1x2n24wbiZV5J49/raw"))()
